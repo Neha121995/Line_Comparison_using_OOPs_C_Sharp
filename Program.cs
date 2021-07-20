0d0008a8429bfa5e66c93cb1773eb303fe5b1209
Neha121995 <jagtapneha1212@gmail.com>
2021-07-20 21:23:56 +0530

    [Neha] Added: Welcome to line comparison

--- Program.cs
+++ Program.cs
@@ -9,7 +9,11 @@ namespace Line_Comparison_using_OOPs
             Console.WriteLine("Welcome to line comparison problem using oops");
             //UC1_CalculateLength.calculateLength();
             //UC2_CheckLinesEqualorNot.calLine();
-            UC3_LinesGreaterOrLesser.calLine();
+            //UC3_LinesGreaterOrLesser.calLine();
+
+            UC4_LineCompareUsingOOPs obj = new UC4_LineCompareUsingOOPs();
+            obj.calLine();
+            obj.Operation();
         }
     }
 }
